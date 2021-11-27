f139ece818ac217db64e4a45922ec6afac4879a7
Dom Mistry <dominicmistry1@gmail.com>
2021-11-27 21:38:18 +0000

    Add swagger annotations to api controller method

--- eatsyAppService/eatsyAppService-controller/src/main/java/org/eatsy/appservice/controller/application/controllers/apiController.java
+++ eatsyAppService/eatsyAppService-controller/src/main/java/org/eatsy/appservice/controller/application/controllers/apiController.java
@@ -1,6 +1,9 @@
 package org.eatsy.appservice.controller.application.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.eatsy.appservice.domain.Recipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,9 +30,11 @@ public class apiController {
      * @param recipeName The name of the recipe the user is adding.
      * @return the recipe object that has been created.
      */
+    @ApiOperation("Returns a new recipe with the information provided in the request")
+    @ApiResponses({ @ApiResponse(code = 200, message = "Successfully created new recipe.") })
     @RequestMapping(value = "/add", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public Recipe addRecipe(final String recipeName) {
+    public Recipe addRecipe(@ApiParam("The recipe name.") final String recipeName) {
 
         logger.debug("A new recipe has been created called " + recipeName);
 
