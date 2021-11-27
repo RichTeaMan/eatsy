6fda87534a708c3cc7ed93d7c589d69144c1802a
Dom Mistry <dominicmistry1@gmail.com>
2021-11-27 22:05:00 +0000

    Fix swagger dependencies for swagger-ui to work

--- eatsyAppService/eatsyAppService-controller/src/main/java/org/eatsy/appservice/controller/application/SpringFoxConfig.java
+++ eatsyAppService/eatsyAppService-controller/src/main/java/org/eatsy/appservice/controller/application/SpringFoxConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Swagger configuration
@@ -19,11 +23,15 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
-                //.apiInfo(apiInfo)
+                .apiInfo(apiInfo)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    final ApiInfo apiInfo = new ApiInfo("Eatsy", "Basic API for Eatsy", "0.1", null, null, "MIT license",
+            "https://github.com/DM1st/eatsy/blob/main/licence.txt",
+            new ArrayList<>());
 
 }
