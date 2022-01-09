342b7de21abf85b45ef5aecffd22c1669d207035
Dom Mistry <dominicmistry1@gmail.com>
2022-01-09 16:27:20 +0000

    Added initial JUnit unit test for RecipeMapper.

---
+++ eatsyAppService/eatsyAppService-service/src/test/java/org/eatsy/appservice/service/RecipeFactoryTest.java
@@ -0,0 +1,20 @@
+package org.eatsy.appservice.service;
+
+import org.junit.Before;
+
+/**
+ * Recipe Factory unit tests
+ */
+public class RecipeFactoryTest {
+
+    /**
+     * Class under test.
+     */
+    private RecipeFactory recipeFactory;
+
+    @Before
+    public void setup() {
+        //recipeFactory = new RecipeFactoryHandler();
+    }
+
+}
