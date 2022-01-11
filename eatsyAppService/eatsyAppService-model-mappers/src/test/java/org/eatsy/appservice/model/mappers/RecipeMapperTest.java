44ec11b7c19dc435c7b161d1b2c499f9ed71e2d0
Dom Mistry <dominicmistry1@gmail.com>
2022-01-11 17:41:28 +0000

    Added unit tests for the Recipe Mapper Handler

--- eatsyAppService/eatsyAppService-model-mappers/src/test/java/org/eatsy/appservice/model/mappers/RecipeMapperTest.java
+++ eatsyAppService/eatsyAppService-model-mappers/src/test/java/org/eatsy/appservice/model/mappers/RecipeMapperTest.java
@@ -68,5 +68,80 @@ public class RecipeMapperTest {
 
     }
 
+    @Test
+    public void checkMapToModelWithEmptyIngredientList() {
+
+        //Setup
+        String recipeName = "Terry's chocolate orange";
+        Map<Integer, String> method = new HashMap<>();
+        method.put(1, "Eat the chocolate orange in one go.");
+        Set<String> ingredientSet = new HashSet<>();
+        Recipe recipe = new Recipe(recipeName, ingredientSet, method);
+
+        //Expectation
+        RecipeModel expectedRecipeModel = new RecipeModel();
+        expectedRecipeModel.setName(recipeName);
+        expectedRecipeModel.setIngredientSet(ingredientSet);
+        expectedRecipeModel.setMethod(method);
+
+        //Test
+        RecipeModel actualRecipeModel = recipeMapper.mapToModel(recipe);
+
+        //Assert
+        Assert.assertEquals(expectedRecipeModel, actualRecipeModel);
+
+    }
+
+    @Test
+    public void checkMapToModelWithEmptyMethod() {
+
+        //Setup
+        String recipeName = "Fish finger sandwich";
+        Set<String> ingredientSet = new HashSet<>();
+        ingredientSet.add("Fish fingers");
+        ingredientSet.add("Bread");
+
+        Map<Integer, String> method = new HashMap<>();
+        Recipe recipe = new Recipe(recipeName, ingredientSet, method);
+
+        //Expectation
+        RecipeModel expectedRecipeModel = new RecipeModel();
+        expectedRecipeModel.setName(recipeName);
+        expectedRecipeModel.setIngredientSet(ingredientSet);
+        expectedRecipeModel.setMethod(method);
+
+        //Test
+        RecipeModel actualRecipeModel = recipeMapper.mapToModel(recipe);
+
+        //Assert
+        Assert.assertEquals(expectedRecipeModel, actualRecipeModel);
+
+    }
+
+    @Test
+    public void checkCantMapModelWithEmptyName() {
+
+        //Setup
+        String recipeName = "   ";
+        Set<String> ingredientSet = new HashSet<>();
+        ingredientSet.add("Eggs");
+        ingredientSet.add("Chips");
+        Map<Integer, String> method = new HashMap<>();
+        method.put(1, "Cook chips");
+        method.put(2, "Fry eggs");
+        method.put(3, "cut up fried eggs over chips");
+        Recipe recipe = new Recipe(recipeName, ingredientSet, method);
+
+        //Expectation - cannot map a recipe domain object with an empty recipeName
+        RecipeModel expectedRecipeModel = null;
+
+        //Test
+        RecipeModel actualRecipeModel = recipeMapper.mapToModel(recipe);
+
+        //Assert
+        Assert.assertEquals(expectedRecipeModel, actualRecipeModel);
+
+    }
+
 
 }
