4c523d05f5e5cfd60a617dfd0e5c40d11c1f3bc2
Dom Mistry <dominicmistry1@gmail.com>
2022-05-24 19:56:40 +0100

    create data generation method for createRecipeModel list

--- eatsyAppService/eatsyAppService-test-data-generation/src/main/java/org/eatsy/appservice/testdatageneration/RecipeModelDataFactory.java
+++ eatsyAppService/eatsyAppService-test-data-generation/src/main/java/org/eatsy/appservice/testdatageneration/RecipeModelDataFactory.java
@@ -2,6 +2,8 @@ package org.eatsy.appservice.testdatageneration;
 
 import org.eatsy.appservice.model.RecipeModel;
 
+import java.util.List;
+
 /**
  * Interface for recipe test data creation
  */
@@ -15,5 +17,15 @@ public interface RecipeModelDataFactory {
      * @return a randomly generated RecipeModel object.
      */
     RecipeModel generateRandomRecipeModel(int maxIngredientSetSize, int maxMethodMapSize);
+
+    /**
+     * Uses the Java Faker library to create a list of RecipeModel objects
+     *
+     * @param maxNumberOfRecipes Max value for the generated number of recipeModels in the list.
+     * @param maxIngredientSetSize Max value for the generated number of ingredients in each recipe
+     * @param maxMethodMapSize     Max value for the generated number of method steps in each recipe
+     * @return a randomly generated list of recipeModel objects.
+     */
+    List<RecipeModel> generateRecipeModelsList(int maxNumberOfRecipes, int maxIngredientSetSize, int maxMethodMapSize);
 
 }
