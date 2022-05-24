dd3adb8e37df88eb09cd97c95955a186668126fa
Dom Mistry <dominicmistry1@gmail.com>
2022-05-24 21:50:54 +0100

    Add interface for RecipeDataFactory

--- eatsyAppService/eatsyAppService-test-data-generation/src/main/java/org/eatsy/appservice/testdatageneration/RecipeModelDataFactory.java
+++ eatsyAppService/eatsyAppService-test-data-generation/src/main/java/org/eatsy/appservice/testdatageneration/RecipeModelDataFactory.java
@@ -5,15 +5,15 @@ import org.eatsy.appservice.model.RecipeModel;
 import java.util.List;
 
 /**
- * Interface for recipe test data creation
+ * Interface for recipe model test data creation
  */
 public interface RecipeModelDataFactory {
 
     /**
      * Uses the Java Faker library to create a RecipeModel object
      *
-     * @param maxIngredientSetSize Max value for the generated number of ingredients in the recipe
-     * @param maxMethodMapSize     Max value for the generated number of method steps in the recipe
+     * @param maxIngredientSetSize Max value for the generated number of ingredients in the recipe model
+     * @param maxMethodMapSize     Max value for the generated number of method steps in the recipe model
      * @return a randomly generated RecipeModel object.
      */
     RecipeModel generateRandomRecipeModel(int maxIngredientSetSize, int maxMethodMapSize);
@@ -21,9 +21,9 @@ public interface RecipeModelDataFactory {
     /**
      * Uses the Java Faker library to create a list of RecipeModel objects
      *
-     * @param maxNumberOfRecipes Max value for the generated number of recipeModels in the list.
-     * @param maxIngredientSetSize Max value for the generated number of ingredients in each recipe
-     * @param maxMethodMapSize     Max value for the generated number of method steps in each recipe
+     * @param maxNumberOfRecipes   Max value for the generated number of recipeModels in the list.
+     * @param maxIngredientSetSize Max value for the generated number of ingredients in each recipe model
+     * @param maxMethodMapSize     Max value for the generated number of method steps in each recipe model
      * @return a randomly generated list of recipeModel objects.
      */
     List<RecipeModel> generateRecipeModelsList(int maxNumberOfRecipes, int maxIngredientSetSize, int maxMethodMapSize);
