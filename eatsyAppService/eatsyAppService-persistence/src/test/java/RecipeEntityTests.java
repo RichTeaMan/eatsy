e2a524220d07294d1c7075cabce6b3bfa0fa7f00
Dom Mistry <dominicmistry1@gmail.com>
2022-11-11 21:10:22 +0000

    EqualsVerify unit tests for RecipeEntity

---
+++ eatsyAppService/eatsyAppService-persistence/src/test/java/RecipeEntityTests.java
@@ -0,0 +1,34 @@
+import com.jparams.verifier.tostring.NameStyle;
+import com.jparams.verifier.tostring.ToStringVerifier;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.eatsy.appservice.persistence.model.RecipeEntity;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Id;
+
+/**
+ * Unit test class for Recipe entity object
+ */
+public class RecipeEntityTests {
+
+    /**
+     * Checks the equals contract of the recipe entity object in case any fields have been missed.
+     */
+    @Test
+    public void TestRecipeEqualsContract() {
+        EqualsVerifier.forClass(RecipeEntity.class)
+                .withIgnoredAnnotations(Id.class)
+                .verify();
+    }
+
+    /**
+     * Checks toStringMethod contains all fields and nothing is missing.
+     */
+    @Test
+    public void testToStringMethod() {
+        ToStringVerifier.forClass(RecipeEntity.class)
+                .withClassName(NameStyle.SIMPLE_NAME)
+                .verify();
+    }
+
+}
