b8967a957e9873159d4bbcb070c6854e9dac9c8d
Dom Mistry <dominicmistry1@gmail.com>
2022-11-22 12:33:53 +0000

    Writing first test

--- eatsyAppService/eatsyAppService-persistence/src/test/java/org/eatsy/appservice/persistence/EatsyRepositoryJPATests.java
+++ eatsyAppService/eatsyAppService-persistence/src/test/java/org/eatsy/appservice/persistence/EatsyRepositoryJPATests.java
@@ -2,8 +2,6 @@ package org.eatsy.appservice.persistence;
 
 import org.eatsy.appservice.persistence.model.RecipeEntity;
 import org.eatsy.appservice.persistence.service.EatsyRepository;
-import org.eatsy.appservice.persistence.service.EatsyRepositoryHandler;
-import org.eatsy.appservice.persistence.service.EatsyRepositoryService;
 import org.eatsy.appservice.testdatageneration.RecipeEntityDataFactory;
 import org.eatsy.appservice.testdatageneration.constants.EatsyRecipeTestParameters;
 import org.junit.jupiter.api.Assertions;
@@ -14,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -26,7 +23,7 @@ import java.util.UUID;
 //The annotation will disable full auto-configuration applying only enable configuration relevant to JPA tests.
 //tests annotated with @DataJpaTest are transactional and roll back at the end of each test
 @DataJpaTest
-public class EatsyRepositoryPersistenceTests {
+public class EatsyRepositoryJPATests {
 
     // TestEntityManager allows us to use EntityManager in DataJpaTests.
     // EntityManager is used by Spring to interact with the persistence context
@@ -36,18 +33,22 @@ public class EatsyRepositoryPersistenceTests {
     //The Eatsy Repository that extends the JPA interface
     @Autowired
     private EatsyRepository eatsyRepository;
-    private EatsyRepositoryService eatsyRepositoryHandler;
+
 
     /**
-     * Setup method before every test = Initialise the class under test
+     * Setup method before every test
      */
     @BeforeEach
     public void setup() {
-        eatsyRepositoryHandler = new EatsyRepositoryHandler();
+
+
     }
 
+    /**
+     * Check the save recipe operations
+     */
     @Test
-    public void checkPersistRecipeTest() {
+    public void checkSaveRecipeOperation() {
 
         //Create test RecipeEntity
         final RecipeEntity expectedRecipeEntity = RecipeEntityDataFactory
@@ -55,16 +56,11 @@ public class EatsyRepositoryPersistenceTests {
         expectedRecipeEntity.setKey(UUID.randomUUID().toString());
 
         //Execute the method under test
-        eatsyRepositoryHandler.persistRecipe(expectedRecipeEntity);
+        final RecipeEntity savedRecipe = eatsyRepository.save(expectedRecipeEntity);
 
-        //Do the assertion
-        final Optional<RecipeEntity> actualEntityOptional = eatsyRepository.findById(expectedRecipeEntity.getKey());
-        if (actualEntityOptional.isPresent()) {
-            final RecipeEntity actualRecipeEntity = actualEntityOptional.get();
-            Assertions.assertEquals(expectedRecipeEntity, actualRecipeEntity);
-        } else {
-            Assertions.fail("The persistRecipe method under test did not work as expected");
-        }
+        //Do the assertions
+        Assertions.assertNotNull(savedRecipe);
+        Assertions.assertFalse(savedRecipe.getKey().isEmpty());
     }
 
 
