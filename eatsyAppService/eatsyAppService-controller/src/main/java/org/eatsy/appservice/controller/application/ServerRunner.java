e2432e84feb307cf23621e04c6ca3cb71f45477e
Dom Mistry <dominicmistry1@gmail.com>
2023-02-25 21:31:17 +0000

    Move single CORS url out to application.properties

--- eatsyAppService/eatsyAppService-controller/src/main/java/org/eatsy/appservice/controller/application/ServerRunner.java
+++ eatsyAppService/eatsyAppService-controller/src/main/java/org/eatsy/appservice/controller/application/ServerRunner.java
@@ -1,9 +1,13 @@
 package org.eatsy.appservice.controller.application;
 
 import org.eatsy.appservice.controller.application.annotations.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,7 +19,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * It also carries out a component scan to find configurations, controllers and services.
  */
 @SpringBootApplication
-public class ServerRunner {
+public class ServerRunner extends SpringBootServletInitializer {
+
+    //For injecting the allowed consumer URLs from the respective (Dev/prod) application.properties
+    @Autowired
+    private Environment env;
 
     /**
      * Entry point of the Spring Boot application
@@ -30,15 +38,19 @@ public class ServerRunner {
      * Configure Cross-Origin Resource Sharing (CORS) in the Spring Boot application
      * CORS needs to be enabled on the server side so that data can be fetched from the React UI
      * <p>
-     * .allowedOrigins("http://localhost:3000", "http://localhost:8080") means the application will only accept requests from
-     * "http://localhost:3000" and "http://localhost:8080"
+     * .allowedOrigins(url) means the application will only accept requests from those listed in the
+     * application.properties such as "http://localhost:3000" and "http://localhost:8080"
      */
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(final CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:8080");
+                final String urls = env.getProperty("cors.urls");
+                final CorsRegistration registration = registry.addMapping("/**");
+                for (final String url : urls.split(",")) {
+                    registration.allowedOrigins(url);
+                }
             }
         };
     }
