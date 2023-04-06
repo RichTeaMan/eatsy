83dea779ada1e2c1ce2e8ce9aadd15954900f609
Dom Mistry <dominicmistry1@gmail.com>
2023-04-06 22:41:51 +0100

    Add recipeImageEntity object

---
+++ eatsyAppService/eatsyAppService-persistence/src/main/java/org/eatsy/appservice/persistence/model/RecipeImageEntity.java
@@ -0,0 +1,45 @@
+package org.eatsy.appservice.persistence.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity class to map the recipeImage object with a corresponding table in the database.
+ */
+//Lombok
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+//persistence
+@Entity
+@Table(name = "recipeImageEntity")
+public class RecipeImageEntity {
+
+    //Primary key
+    @Id
+    @Column(name = "key")
+    private String key;
+
+    //Image name.
+    @Column(name = "imageName")
+    private String imageName;
+
+    //type of file e.g image/jpeg
+    @Column(name = "imageType")
+    private String imageType;
+
+    //image bytes can have large lengths so we specify a value
+    //which is more than the default length for picByte column
+    @Column(name = "picByte", length = 1000)
+    private byte[] picByte;
+
+
+}
